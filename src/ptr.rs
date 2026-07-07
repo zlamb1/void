2291ada10120df3dba7b7d3d18bb08a5042ee066
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2026-07-07 14:04:04 -0500

    Introduced container_of_mut! macro.

--- src/ptr.rs
+++ src/ptr.rs
@@ -6,3 +6,12 @@ macro_rules! container_of {
         ptr.cast::<$Container>()
     }};
 }
+
+#[macro_export]
+macro_rules! container_of_mut {
+    ($field_ptr:expr, $Container:ty, $field:ident) => {{
+        let ptr = $field_ptr as *const _ as *const u8;
+        let ptr = ptr.wrapping_sub(core::mem::offset_of!($Container, $field));
+        ptr.cast_mut::<$Container>()
+    }};
+}
