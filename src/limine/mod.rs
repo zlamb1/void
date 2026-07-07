f2a60ecf2439e8c375c786c059a294aca6e7014a
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2026-07-07 15:05:04 -0500

    Now print warning about framebuffer not being available instead of panicking.

--- src/limine/mod.rs
+++ src/limine/mod.rs
@@ -2,8 +2,8 @@ use core::cell::OnceCell;
 use core::pin::Pin;
 use core::ptr::NonNull;
 
-use crate::gfx;
 use crate::sync::SpinLock;
+use crate::{gfx, println};
 
 pub mod fb;
 
@@ -40,25 +40,29 @@ static FB_CONSOLE: SpinLock<OnceCell<gfx::fb::Console>> = SpinLock::new(OnceCell
 
 pub fn init() {
     let fb = FB_REQUEST.with(|req| {
-        let response = unsafe { req.response.unwrap().as_ref() };
-        let fb = unsafe { (*response.framebuffers.unwrap().as_ptr()).unwrap().as_ref() };
+        let fb = unsafe {
+            let response = req.response?.as_ref();
+            (*response.framebuffers?.as_ptr())?.as_ref()
+        };
         gfx::fb::Fb::try_new(
             fb.address.cast(),
-            fb.width.try_into().unwrap(),
-            fb.height.try_into().unwrap(),
-            fb.pitch.try_into().unwrap(),
+            fb.width.try_into().ok()?,
+            fb.height.try_into().ok()?,
+            fb.pitch.try_into().ok()?,
             fb.bpp,
             gfx::fb::Mask::new(fb.red_mask.size, fb.red_mask.shift),
             gfx::fb::Mask::new(fb.green_mask.size, fb.green_mask.shift),
             gfx::fb::Mask::new(fb.blue_mask.size, fb.blue_mask.shift),
         )
-        .unwrap()
     });
-    FB_CONSOLE.with_mut(|console| {
+    if let Some(fb) = fb {
+        let console = FB_CONSOLE.acquire();
         console
             .set(gfx::fb::Console::new(fb, gfx::font::terminus16_8::FONT))
             .unwrap();
         let console = unsafe { Pin::new_unchecked(console.get().unwrap_unchecked().base()) };
         crate::log::register(console);
-    });
+    } else {
+        println!("framebuffer console not supported");
+    }
 }
